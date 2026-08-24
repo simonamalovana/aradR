5f818f538c66e319e2a03eb8f836d4c363a6c46a
simonamalovana <168445327+simonamalovana@users.noreply.github.com>
2026-08-24 17:34:25 +0200

    Add package documentation stub

---
+++ R/aradR-package.R
@@ -0,0 +1,7 @@
+#' aradR: Modern R Client and Toolkit for the Czech National Bank ARAD API
+#'
+#' `aradR` provides a reliability-first interface for discovering, retrieving,
+#' validating, and working with data from the Czech National Bank ARAD API.
+#'
+#' @keywords internal
+"_PACKAGE"
